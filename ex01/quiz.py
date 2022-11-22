8d48b8e6313eefd354e79f1836ba549bed5cbd34
yamamoto <c0a2113791@edu.teu.ac.jp>
2022-11-22 16:07:00 +0900

    完成

---
+++ ex01/quiz.py
@@ -0,0 +1,15 @@
+import random
+Quiz=[
+    ["サザエの旦那の名前は？","マスオ","ますお"],
+    ["カツオの妹の名前は？","わかめ","ワカメ"],
+    ["タラオはカツオから見てどんな関係？","甥","おい","甥っ子","おいっこ"]
+    ]
+
+def shutudai():
+    print(Quiz[Quiznnum][0])
+    Quiznum = random.randint(0.2)
+def kaitou():
+    ans = input("")
+shutudai()
+
+
