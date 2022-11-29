5c02b0001f2553945b307c1d4dc4e599a79bfebc
yamamoto <c0a2113791@edu.teu.ac.jp>
2022-11-29 14:35:35 +0900

    4

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,9 +6,14 @@ def button_click(event):
     btn = event.widget
     i = btn["text"]
     tkm.showinfo("", f"{i}ボタンが押されました")
+#1
 root = tk.Tk()
 root.geometry("300x500")
-r,c = 0,0
+#4
+entry = tk.Entry(root, justify="right", width=10,font=("",40))
+entry.grid(row=0, column= 0,columnspan=3)
+
+r,c = 1,0
 for i in range(9,-1,-1):
     button = tk.Button(root, text=f"{i}",width=4,height=2,font=(" ",30))
     button.grid(row=r,column=c)
