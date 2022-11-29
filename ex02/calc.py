fe3682541db02e058410e64e071ed08cf9e5b128
yamamoto <c0a2113791@edu.teu.ac.jp>
2022-11-29 14:40:29 +0900

    5

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,12 +12,21 @@ root.geometry("300x500")
 #4
 entry = tk.Entry(root, justify="right", width=10,font=("",40))
 entry.grid(row=0, column= 0,columnspan=3)
-
+#2
 r,c = 1,0
 for i in range(9,-1,-1):
     button = tk.Button(root, text=f"{i}",width=4,height=2,font=(" ",30))
     button.grid(row=r,column=c)
     button.bind("<1>",button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1 
+        c = 0
+
+op = ["+","="]
+for ope in op:
+    button = tk.Button(root, text=f"{ope}",width=4,height=2,font=(" ",30))
+    button.grid(row=r ,column=c)
     c += 1
     if c%3 == 0:
         r += 1 
