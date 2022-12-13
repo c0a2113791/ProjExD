d3b381a9db79ae20a8f8529b2bf5873ecf15934f
yamamoto <c0a2113791@edu.teu.ac.jp>
2022-12-13 18:07:50 +0900

    対処 #15

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -15,7 +15,7 @@ def check_bound(obj_rct, scr_rct):
 def main():
     clock = pg.time.Clock()
     
-    font = pg.font.Font(None,100)#文字フォント設定
+    font = pg.font.Font(None,100)   #文字フォント設定
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1500,800))
     scrn_rct = scrn_sfc.get_rect()
@@ -32,7 +32,7 @@ def main():
     tori2_rct = tori2_sfc.get_rect()
     tori2_rct.center = 500,250
 
-    bomb_sfc = pg.Surface((60,60))#正方形のからのサーフェイス
+    bomb_sfc = pg.Surface((60,60))  #正方形のからのサーフェイス
     bomb_sfc.set_colorkey((0,0,0))
     pg.draw.circle(bomb_sfc,(255,0,0), (30,30),10)#整形
     bomb_rct = bomb_sfc.get_rect()
@@ -44,7 +44,7 @@ def main():
     while True :
         scrn_sfc.blit(pgbg_sfc,pgbg_rct)
         for event in pg.event.get():
-            if event.type == pg.QUIT:#ウィンドウの「ｘ」が押されたら終了
+            if event.type == pg.QUIT:   #ウィンドウの「ｘ」が押されたら終了
                 return
         key_dic = pg.key.get_pressed()#キーが押された時の動作
         #右に表示されるこうかとん(p1)の操作
@@ -88,26 +88,26 @@ def main():
         scrn_sfc.blit(tori_sfc,tori_rct)
         scrn_sfc.blit(tori2_sfc,tori2_rct)
         
-        bomb_rct.move_ip(vx,vy)#爆弾位置情報
+        bomb_rct.move_ip(vx,vy) #爆弾位置情報
         scrn_sfc.blit(bomb_sfc,bomb_rct)
         yoko,tate = check_bound(bomb_rct,scrn_rct)
         vx *= yoko
         vy *= tate
 
-        if  tori_rct.colliderect(bomb_rct) or tori2_rct.colliderect(bomb_rct):#1p,2pが爆弾に接触すると次のwhile文に入る
+        if  tori_rct.colliderect(bomb_rct) or tori2_rct.colliderect(bomb_rct):  #1p,2pが爆弾に接触すると次のwhile文に入る
             while True:
-                txt = font.render("GAME OVER",True,(0,0,0))#ゲームオーバー表示
+                txt = font.render("GAME OVER",True,(0,0,0)) #ゲームオーバー表示
                 scrn_sfc.blit(txt,(610,400))
                 pg.display.update()
                 for event in pg.event.get():
-                    if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:#エスケープキーを押したら終了
+                    if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:   #エスケープキーを押したら終了
                         return
-                    if event.type == pg.QUIT:#ウィンドウの「ｘ」が押されたら終了
+                    if event.type == pg.QUIT:   #ウィンドウの「ｘ」が押されたら終了
                         return
 
         pg.display.update()
         clock.tick(1000)
-        
+
 if __name__ == "__main__":
     pg.init()
     main()
